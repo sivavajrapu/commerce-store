9dc5e1da16b8365dd51982adf8569f0cf441d24f
sivavajrapu <vajrapu.siva@gmail.com>
2022-11-07 10:39:04 +0530

    removed cart and remaing fles

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import Cart from './components/pages/Cart/Cart'
 import Home from './components/pages/Home/Home'
-import MyProfile from './components/pages/MyProfile/MyProfile'
-import NavBar from './components/pages/NavBar/NavBar'
-import ProductDetails from './components/pages/ProductDetails/ProductDetails'
 
 export default function App() {
   return (
     <>
-      <NavBar />
       <Routes>
         <Route path='/' element={<Home />}></Route>
-        <Route path='/product_detail' element={<ProductDetails />}></Route>
-        <Route path='/cart' element={<Cart/>}></Route>
-        <Route path='/myProfile' element={<MyProfile/>}></Route>
       </Routes>
     </>
   )
--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,36 +1,53 @@
-import React,{useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { BASE_URL } from '../../../constants';
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import Spinner from '../../Helpers/Spinner/Spinner';
 export default function Home() {
-    const [categories, setCategories] = useState([])
+    const [data, setData] = useState([])
+    const [loader, setLoader] = useState(true)
     const navigate = useNavigate();
-    useEffect(()=>{
-        axios.get(`${BASE_URL}/products/categories`)
-        .then((res)=>{setCategories(res?.data)})
-        .catch((err)=>console.log(JSON.stringify(err)))
-    },[])
+    useEffect(() => {
+        axios.get(`${BASE_URL}/users`)
+            .then((res) => { setLoader(false); setData(res?.data); console.log(JSON.stringify(res?.data)) })
+            .catch((err) => console.log(JSON.stringify(err)))
+    }, [])
 
     const productDetails = (e) => {
-         navigate('/product_detail', {state:{
-            name : e
-         }})
+        navigate('/product_detail', {
+            state: {
+                name: e
+            }
+        })
     }
-  return (
-    <div>
-            <p className='text-center text-2xl'>All Categories</p>
-            <div className='flex justify-center mt-4'>
-                {categories.map((item,i)=>{
-                    return(
-                        <div key={i} onClick={()=> {productDetails(item)}}>
-                            <div className='w-[200px] bg-orange-400 m-3 px-7 py-7 cursor-pointer'>
-                                <p className='text-center'>{item}</p>
-                                
-                            </div>
-                        </div>
-                    )
-                })}
-            </div>
-    </div>
-  )
+    return (
+        <div className='bg-slate-300 h-screen'>
+            {
+                loader === true ?
+                    <Spinner /> :
+                    <div >
+                        {data.map((item, i) => {
+                            return (
+                                <div key={i} className='card rounded-none mx-2 mb-2 flex'>
+                                    <div>
+                                        <img src={`https://avatars.dicebear.com/v2/avataaars/${item.name}.svg?options[mood][]=happy`} className="w-[150px] h-[150px]"/>
+                                    </div>
+                                    <div className='ml-4'>
+                                        <p className='text-xl font-semibold'>{item.name}</p>
+                                        <p className='text-sm font-bold'>Email: <span className='font-normal'>{item.email}</span></p>
+                                        <p className='text-sm font-bold'>Phone: <span className='font-normal'>{item.phone}</span></p>
+                                        <p className='text-sm font-bold'>Company: <span className='font-normal'>{item.company.name}</span></p>
+                                        <p className='text-sm font-bold'>Website: <span className='font-normal'>{item.website}</span></p>
+                                        <p className='text-sm font-bold'>Address: <span className='font-normal'>{item.address.street},{item.address.suite},{item.address.zipcode}</span></p>
+                                    </div>
+
+                                </div>
+
+                            )
+                        })}
+                    </div>
+            }
+
+        </div>
+    )
 }
